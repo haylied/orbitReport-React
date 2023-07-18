6f9614daea0afd8099fa3ab9cdb166dcadab65be
haylied <hayliedarland@gmail.com>
2023-07-18 15:02:56 -0500

    button.js reconfigured

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,19 +1,16 @@
 import satData from "./satData"
 
-const Buttons = (filterByType, setSat, displaySats) => {
-return (
-<div>
+const Buttons = (app.filterByType, app.setSat, app.displaySats) => {
+
   {displaySats.map((sat, id) => {
     return (
       <button onClick={() => filterByType(sat)} key={id}>
         {sat} Orbit
         </button>
     );
-  })}
-      <button onClick={() => setSat(satData)}>All Orbits
-      </button>
-  </div>
-)
+  })
+}<button onClick={() => setSat(satData)}>All Orbits
+</button>
 }
 
 export default Buttons;
